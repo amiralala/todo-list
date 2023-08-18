5b398c04bd8e782389470e0ae72654b96b00bae0
amira <fathallah_amira@yahoo.fr>
2023-08-18 08:08:48 +0200

    test @ alias

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Welcome from '../views/Welcome.vue'
+import Welcome from '@/views/Welcome.vue'
 import GeneraltasksUser from '../views/GeneraltasksUser.vue'
 import TaskEdit from '../components/TaskEdit.vue'
  
